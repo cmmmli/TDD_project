c0aa37832ae7be2d1ca84d598235b577f3fd3a47
komoriyuichiro <thekomori1113@gmail.com>
2018-06-19 17:01:26 +0900

    chapter12

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression {
 	protected int amount;
 	protected String currency;
 	Money(int amount, String currency) {
@@ -9,6 +9,9 @@ class Money {
 	}
 	Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
+	}
+	Expression plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
 	}
 	String currency() {
 		return currency;
@@ -25,6 +28,6 @@ class Money {
 		return new Money(amount, "USD");
 	}
 	static Money franc(int amount) {
-		return new Money(amount, "CHE");
+		return new Money(amount, "CHF");
 	}
 }
